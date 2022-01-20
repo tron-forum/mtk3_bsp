8be426d9ae9d88e4acc95a4dccaa6c0a940193eb
Yuichi Toyoyama <yuichi.toyoyama@iniad.org>
2022-01-20 15:59:44 +0900

    物理タイマ&シリアル通信デバイス変更

--- device/ser/sysdepend/stm32l4/ser_cnf_stm32l4.h
+++ device/ser/sysdepend/stm32l4/ser_cnf_stm32l4.h
@@ -1,12 +1,12 @@
 ﻿/*
  *----------------------------------------------------------------------
- *    Device Driver for micro T-Kernel for μT-Kernel 3.00.03
+ *    Device Driver for micro T-Kernel for μT-Kernel 3.00
  *
  *    Copyright (C) 2020-2021 by Ken Sakamura.
  *    This software is distributed under the T-License 2.2.
  *----------------------------------------------------------------------
  *
- *    Released by TRON Forum(http://www.tron.org) at 2021/03/31.
+ *    Released by TRON Forum(http://www.tron.org) at 2022/02.
  *
  *----------------------------------------------------------------------
  */
@@ -21,17 +21,20 @@
 #define	__DEV_SER_CNF_STM32L4_H__
 
 /* Device initialization */
-#define DEVCONF_SER_INIT_MCLK	FALSE		// Initialize module clock
+#define DEVCONF_SER_INIT_MCLK	TRUE		// Initialize module clock
 
 /* Device control data */
-#define	DEVCNF_SER_INTPRI	5		// Interrupt priority
+//#define	DEVCNF_SER_INTPRI	5		// Interrupt priority
+
+#define	DEVCNF_USART1_INTPRI	5
+#define	DEVCNF_USART2_INTPRI	5
+#define	DEVCNF_USART3_INTPRI	5
+#define	DEVCNF_UART4_INTPRI	5
+#define	DEVCNF_UART5_INTPRI	5
 
 /* Debug option
  *	Specify the device used by T-Monitor.
- *	  0: "sera" - USART1
- *	  1: "serb" - USART2
- *	  2: "serc" - USART3
- *	  other : T-Monitor does not use serial devices
+ *	  -1 : T-Monitor does not use serial devices
  */
 #if USE_TMONITOR
 #define	DEVCNF_SER_DBGUN	1		// Used by T-Monitor
