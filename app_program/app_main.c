94e758e35146d5228df36eecf00f5d08e5c448ec
tron-forum <tkernel_dev@tron.org>
2022-02-14 16:34:02 +0900

    Build a project

--- app_program/app_main.c
+++ app_program/app_main.c
@@ -1,10 +1,50 @@
+/*
+ *----------------------------------------------------------------------
+ *    micro T-Kernel 3.0 BSP
+ *
+ *    Copyright (C) 2022 by Ken Sakamura.
+ *    This software is distributed under the T-License 2.2.
+ *----------------------------------------------------------------------
+ *
+ *    Released by TRON Forum(http://www.tron.org) at 2022/02.
+ *
+ *----------------------------------------------------------------------
+ */
+
+/*
+ *	app_main.c (Nucleo-144 STM32H723)
+ *	Application main program
+ */
+
 #include <tk/tkernel.h>
 #include <tm/tmonitor.h>
+
+LOCAL void task_1(INT stacd, void *exinf);
+LOCAL ID	tskid_1;
+LOCAL T_CTSK	ctsk_1 = {
+	.itskpri	= 10,
+	.stksz		= 1024,
+	.task		= task_1,
+	.tskatr		= TA_HLNG | TA_RNG3,
+};
+
+LOCAL void task_1(INT stacd, void *exinf)
+{
+	while(1) {
+		out_w(GPIO_ODR(B), (in_w(GPIO_ODR(B)))^((1<<0)|(1<<14)));
+		out_w(GPIO_ODR(E), (in_w(GPIO_ODR(E)))^(1<<1));
+		tk_dly_tsk(500);
+	}
+}
 
 /* usermain関数 */
 EXPORT INT usermain(void)
 {
 	tm_printf((UB*)"User program started\n");
 
+	tskid_1 = tk_cre_tsk(&ctsk_1);
+	tk_sta_tsk(tskid_1, 0);
+
+	tk_slp_tsk(TMO_FEVR);
 	return 0;
 }
