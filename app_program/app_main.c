5a1ead6a8e6b9e3c20e4e741c66899aab32d8637
Yuichi Toyoyama <yuichi.toyoyama@iniad.org>
2023-05-24 16:54:48 +0900

    Create Project

--- app_program/app_main.c
+++ app_program/app_main.c
@@ -2,18 +2,18 @@
  *----------------------------------------------------------------------
  *    micro T-Kernel 3.0 BSP
  *
- *    Copyright (C) 2022 by Ken Sakamura.
+ *    Copyright (C) 2022-2023 by Ken Sakamura.
  *    This software is distributed under the T-License 2.2.
  *----------------------------------------------------------------------
  *
- *    Released by TRON Forum(http://www.tron.org) at 2022/11.
+ *    Released by TRON Forum(http://www.tron.org) at 2023/05.
  *
  *----------------------------------------------------------------------
  */
 
 /*
  *	app_main.c
- *	Application main program
+ *	Application main program for RaspberryPi Pico
  */
 
 #include <tk/tkernel.h>
